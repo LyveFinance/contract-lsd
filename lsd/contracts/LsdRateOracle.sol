226dc44d692ac461899757cfe5d97d90e61cf93f
LyveFinance <140053171+LyveFinance@users.noreply.github.com>
2023-07-25 17:26:20 +0800

    code update
    code update

--- lsd/contracts/LsdRateOracle.sol
+++ lsd/contracts/LsdRateOracle.sol
@@ -6,15 +6,16 @@ import {ILsdRateOracle} from "./interfaces/ILsdRateOracle.sol";
 
 contract LsdRateOracle is ILsdRateOracle ,ReentrancyGuard{
     mapping(address => uint256) private rates; 
+    address public owner;
     constructor() {
-        
+        owner = msg.sender;
     }
 
     function getLsdRate(address _lsdToken) view external returns(uint256){
         return rates[_lsdToken];
     }
     function setLsdRate(address _lsdToken,uint256 _rate) external nonReentrant {
-        //todo controller
+         require(msg.sender == owner,"only owner" );
         uint256 rate = rates[_lsdToken] ;
         require(_rate >= 1e18,"wrong _rate" );
         require(_rate > rate,"wrong _rate" );
